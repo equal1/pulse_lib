04d9c45da64a80b99ec6670bc0f74decd61336fc
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2025-02-17 10:50:17 +0100

    Added exception when acquisition time is too short for low-pass filter

--- pulse_lib/qblox/filtering.py
+++ pulse_lib/qblox/filtering.py
@@ -6,12 +6,17 @@ from functools import cache
 def low_pass_window(n: int):
     """ Creates a window with incorporated low pass filter.
 
+    The filter attenuates frequences above 95 MHz with 40 dB. This is on top
+    of the attenuation due to the rectangular integration window.
+
     Args:
         n: length of window.
 
-    With n=200 all frequencies above 95 MHz are attenuated with > 81 dB.
-    With n=1000 they are attenuated with > 95 dB.
-    With n=2000 they are attenuated with > 101 dB, i.e. < 1/100_000 of original amplitude.
+    The cummulative effect of the rectangular window and the low pass filter:
+        n=100: frequencies above 95 MHz are attenuated with > 75 dB.
+        n=200: frequencies above 95 MHz are attenuated with > 81 dB.
+        n=1000: frequencies above 95 MHz are attenuated with > 95 dB.
+        With n=2000 they are attenuated with > 101 dB, i.e. < 1/100_000 of original amplitude.
 
     Notes:
         Maximum length for Qblox acquisition weight is 16384 samples.
@@ -21,6 +26,8 @@ def low_pass_window(n: int):
         already attenuated frequencies above 100 MHz with 74 dB, i.e.
         1/5000 of input amplitude.
     """
+    if n < 40:
+        raise Exception(f"Acquisition time of {n} ns is shorter than minimum of 40 ns when low-pass filter on.")
     ramp = [
         0.00458420, 0.01575104, 0.03271758, 0.05871736, 0.09609971,
         0.14609971, 0.20871736, 0.28271758, 0.36575104, 0.45458420,
