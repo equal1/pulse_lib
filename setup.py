3b52c7546450b137c80e95748b8dab02781f8e40
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2025-08-05 16:48:31 +0200

    Bump to 1.7.61

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ print('packages: %s' % packages)
 
 
 setup(name="pulse_lib",
-	version="1.7.60",
+	version="1.7.61",
 	packages = find_packages(),
     python_requires=">=3.10",
     install_requires=[
